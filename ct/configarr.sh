5170513122b837d41a2d815f4ac0d6e1634eb554
finkerle <fatihakcay@live.de>
2025-05-04 17:05:16 +0200

    configured scripts for configarr install

--- ct/configarr.sh
+++ ct/configarr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/finkerle/ProxmoxVED/refs/heads/$APP-new/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/finkerle/refs/heads/configarr-new/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: [YourUserName]
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -25,41 +25,41 @@ function update_script() {
     check_container_resources
 
     # Check if installation is present | -f for file, -d for folder
-    if [[ ! -d /opt/$APP ]]; then
+    if [[ ! -d /opt/configarr ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
 
     # Crawling the new version and checking whether an update is required
-    RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/$APP/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+    RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/configarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 
-    if [[ "${RELEASE}" != "$(cat /opt/$APP_version.txt)" ]] || [[ ! -f /opt/$APP_version.txt ]]; then
+    if [[ "${RELEASE}" != "$(cat /opt/configarr_version.txt)" ]] || [[ ! -f /opt/configarr_version.txt ]]; then
         # Stopping Services
         msg_info "Stopping $APP"
-        systemctl stop $APP-task.timer
-        systemctl stop $APP-task.service
+        systemctl stop configarr-task.timer
+        systemctl stop configarr-task.service
         msg_ok "Stopped $APP"
 
         # Execute Update
         msg_info "Updating $APP to v${RELEASE}"
         temp_file=$(mktemp)
-        curl -fsSL "https://github.com/raydak-labs/$APP/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
+        curl -fsSL "https://github.com/raydak-labs/configarr/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
         unzip -q $temp_file
-        rm -rf /opt/$APP
-        mv "$APP-${RELEASE}/" /opt/$APP
-        cd /opt/$APP
+        rm -rf /opt/configarr
+        mv "configarr-${RELEASE}/" /opt/configarr
+        cd /opt/configarr
         pnpm install
         pnpm run build
         msg_ok "Updated $APP to v${RELEASE}"
 
         # Starting Services
         msg_info "Starting $APP"
-        systemctl start $APP-task.timer
-        systemctl start $APP-task.service
-        msg_ok "Started $APP"
+        systemctl start configarr-task.timer
+        systemctl start configarr-task.service
+        msg_ok "Started configarr"
 
         # Last Action
-        echo "${RELEASE}" >/opt/$APP_version.txt
+        echo "${RELEASE}" >/opt/configarr_version.txt
         msg_ok "Update Successful"
     else
         msg_ok "No update required. ${APP} is already at v${RELEASE}"
