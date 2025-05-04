2f3a27b8697b6002c5cc50844def80691c589f92
finkerle <fatihakcay@live.de>
2025-05-04 16:50:13 +0200

    configured scripts for configarr install

--- ct/configarr.sh
+++ ct/configarr.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/finkerle/ProxmoxVED/refs/heads/configarr-new/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/finkerle/ProxmoxVED/refs/heads/$APP-new/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: [YourUserName]
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: [SOURCE_URL]
 
-# App Default Values
-APP="Configarr"
-# Name of the app (e.g. Google, Adventurelog, Apache-Guacamole"
+APP="$APP"
 var_tags="arr"
-# Tags for Proxmox VE, maximum 2 pcs., no spaces allowed, separated by a semicolon ; (e.g. database | adblock;dhcp)
 var_cpu="1"
-# Number of cores (1-X) (e.g. 4) - default are 2
 var_ram="512"
-# Amount of used RAM in MB (e.g. 2048 or 4096)
 var_disk="4"
-# Amount of used disk space in GB (e.g. 4 or 10)
 var_os="debian"
-# Default OS (e.g. debian, ubuntu, alpine)
 var_version="12"
-# Default OS version (e.g. 12 for debian, 24.04 for ubuntu, 3.20 for alpine)
 var_unprivileged="1"
-# 1 = unprivileged container, 0 = privileged container
 
 header_info "$APP"
 variables
@@ -34,41 +25,41 @@ function update_script() {
     check_container_resources
 
     # Check if installation is present | -f for file, -d for folder
-    if [[ ! -d /opt/configarr ]]; then
+    if [[ ! -d /opt/$APP ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
 
     # Crawling the new version and checking whether an update is required
-    RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/configarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+    RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/$APP/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 
-    if [[ "${RELEASE}" != "$(cat /opt/configarr_version.txt)" ]] || [[ ! -f /opt/configarr_version.txt ]]; then
+    if [[ "${RELEASE}" != "$(cat /opt/$APP_version.txt)" ]] || [[ ! -f /opt/$APP_version.txt ]]; then
         # Stopping Services
         msg_info "Stopping $APP"
-        systemctl stop configarr-task.timer
-        systemctl stop configarr-task.service
+        systemctl stop $APP-task.timer
+        systemctl stop $APP-task.service
         msg_ok "Stopped $APP"
 
         # Execute Update
         msg_info "Updating $APP to v${RELEASE}"
         temp_file=$(mktemp)
-        curl -fsSL "https://github.com/raydak-labs/configarr/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
+        curl -fsSL "https://github.com/raydak-labs/$APP/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
         unzip -q $temp_file
-        rm -rf /opt/configarr
-        mv "configarr-${RELEASE}/" /opt/configarr
-        cd /opt/configarr
+        rm -rf /opt/$APP
+        mv "$APP-${RELEASE}/" /opt/$APP
+        cd /opt/$APP
         pnpm install
         pnpm run build
         msg_ok "Updated $APP to v${RELEASE}"
 
         # Starting Services
         msg_info "Starting $APP"
-        systemctl start configarr-task.timer
-        systemctl start configarr-task.service
+        systemctl start $APP-task.timer
+        systemctl start $APP-task.service
         msg_ok "Started $APP"
 
         # Last Action
-        echo "${RELEASE}" >/opt/configarr_version.txt
+        echo "${RELEASE}" >/opt/$APP_version.txt
         msg_ok "Update Successful"
     else
         msg_ok "No update required. ${APP} is already at v${RELEASE}"
