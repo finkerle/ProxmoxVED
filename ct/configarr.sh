bb967b7fce13ba73a7df83ffd7572d4bf1543de9
finkerle <fatihakcay@live.de>
2025-05-04 16:26:26 +0200

    configured scripts for configarr install

--- ct/configarr.sh
+++ ct/configarr.sh
@@ -6,21 +6,21 @@ source <(curl -fsSL https://raw.githubusercontent.com/finkerle/ProxmoxVED/refs/h
 # Source: [SOURCE_URL]
 
 # App Default Values
-APP="[APP_NAME]"
+APP="Configarr"
 # Name of the app (e.g. Google, Adventurelog, Apache-Guacamole"
-var_tags="[TAGS]"
+var_tags="arr"
 # Tags for Proxmox VE, maximum 2 pcs., no spaces allowed, separated by a semicolon ; (e.g. database | adblock;dhcp)
-var_cpu="[CPU]"
+var_cpu="1"
 # Number of cores (1-X) (e.g. 4) - default are 2
-var_ram="[RAM]"
+var_ram="512"
 # Amount of used RAM in MB (e.g. 2048 or 4096)
-var_disk="[DISK]"
+var_disk="4"
 # Amount of used disk space in GB (e.g. 4 or 10)
-var_os="[OS]"
+var_os="debian"
 # Default OS (e.g. debian, ubuntu, alpine)
-var_version="[VERSION]"
+var_version="12"
 # Default OS version (e.g. 12 for debian, 24.04 for ubuntu, 3.20 for alpine)
-var_unprivileged="[UNPRIVILEGED]"
+var_unprivileged="1"
 # 1 = unprivileged container, 0 = privileged container
 
 header_info "$APP"
@@ -34,41 +34,41 @@ function update_script() {
     check_container_resources
 
     # Check if installation is present | -f for file, -d for folder
-    if [[ ! -f [INSTALLATION_CHECK_PATH] ]]; then
+    if [[ ! -d /opt/configarr ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
 
     # Crawling the new version and checking whether an update is required
-    RELEASE=$(curl -fsSL [RELEASE_URL] | [PARSE_RELEASE_COMMAND])
-    if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]] || [[ ! -f /opt/${APP}_version.txt ]]; then
+    RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/configarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+
+    if [[ "${RELEASE}" != "$(cat /opt/configarr_version.txt)" ]] || [[ ! -f /opt/configarr_version.txt ]]; then
         # Stopping Services
         msg_info "Stopping $APP"
-        systemctl stop [SERVICE_NAME]
+        systemctl stop configarr-task.timer
+        systemctl stop configarr-task.service
         msg_ok "Stopped $APP"
-
-        # Creating Backup
-        msg_info "Creating Backup"
-        tar -czf "/opt/${APP}_backup_$(date +%F).tar.gz" [IMPORTANT_PATHS]
-        msg_ok "Backup Created"
 
         # Execute Update
         msg_info "Updating $APP to v${RELEASE}"
-        [UPDATE_COMMANDS]
+        temp_file=$(mktemp)
+        curl -fsSL "https://github.com/raydak-labs/configarr/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
+        unzip -q $temp_file
+        rm -rf /opt/configarr
+        mv "configarr-${RELEASE}/" /opt/configarr
+        cd /opt/configarr
+        pnpm install
+        pnpm run build
         msg_ok "Updated $APP to v${RELEASE}"
 
         # Starting Services
         msg_info "Starting $APP"
-        systemctl start [SERVICE_NAME]
+        systemctl start configarr-task.timer
+        systemctl start configarr-task.service
         msg_ok "Started $APP"
 
-        # Cleaning up
-        msg_info "Cleaning Up"
-        rm -rf [TEMP_FILES]
-        msg_ok "Cleanup Completed"
-
         # Last Action
-        echo "${RELEASE}" >/opt/${APP}_version.txt
+        echo "${RELEASE}" >/opt/configarr_version.txt
         msg_ok "Update Successful"
     else
         msg_ok "No update required. ${APP} is already at v${RELEASE}"
---
+++ install/configarr-install.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: finkerle
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/raydak-labs/configarr
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+    git
+msg_ok "Installed Dependencies"
+
+msg_info "Setting up Node.js Repository"
+$STD mkdir -p /etc/apt/keyrings
+$STD curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+$STD echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
+$STD npm install -g pnpm
+msg_ok "Installed Node.js"
+
+msg_info "Installing Configarr"
+
+temp_file=$(mktemp)
+RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/configarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+curl -fsSL "https://github.com/raydak-labs/configarr/archive/refs/tags/v${RELEASE}.zip" -o $temp_file
+$STD unzip -q $temp_file -d /opt/configarr
+
+msg_info "Setup ${APPLICATION}"
+mkdir -p /opt/configarr/repos
+mkdir -p /opt/configarr/templates
+cat <<EOF >/etc/configarr/.env
+ROOT_PATH=/opt/configarr
+CUSTOM_REPO_ROOT=/etc/configarr/repos
+CONFIG_LOCATION=/etc/configarr/config.yml
+SECRETS_LOCATION=/etc/configarr/secrets.yml
+#DRY_RUN=true # not fully supported yet
+#LOAD_LOCAL_SAMPLES=false
+#DEBUG_CREATE_FILES=false
+#LOG_LEVEL=info
+EOF
+mv /opt/configarr/secrets.yml.template /etc/configarr/secrets.yml
+sed 's|#localConfigTemplatesPath: /app/templates|#localConfigTemplatesPath: /opt/configarr/templates|' config.yml.template >/etc/configarr/config.yml
+cd /opt/configarr
+$STD pnpm install
+$STD pnpm run build
+echo "${RELEASE}" >/opt/configarr_version.txt
+msg_ok "Setup ${APPLICATION}"
+
+# Creating Service (if needed)
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/configarr-task.service
+
+[Unit]
+Description=Run Configarr Task
+
+[Service]
+Type=oneshot
+WorkingDirectory=/opt/configarr
+ExecStart=/usr/bin/node /opt/configarr/bundle.cjs
+
+EOF
+cat <<EOF >/etc/systemd/system/configarr-task.timer
+
+[Unit]
+Description=Run Configarr every 5 minutes
+
+[Timer]
+OnBootSec=2min
+OnUnitActiveSec=5min
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+
+EOF
+systemctl enable -q --now configarr-task.timer
+systemctl enable -q --now configarr-task.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+# Cleanup
+msg_info "Cleaning up"
+rm -f $temp_file
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
